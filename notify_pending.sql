63e8562ae4eb6d4081f6b5c004057bad6eacdf2b
Dragusin Mihai <chaotikdarkness@gmail.com>
2022-01-23 00:03:01 +0800

    notify_pending loop modification

--- notify_pending.sql
+++ notify_pending.sql
@@ -1,18 +1,14 @@
 create or replace function notify_pending(ch varchar(255)) 
-returns varchar(255) as
+returns SETOF integer as
 $$
 declare eventId integer;
-declare i integer = 0;
-declare return_message varchar(255);
 BEGIN
-FOR COUNTER IN 1..100 LOOP
-SELECT id INTO eventId FROM notify_events WHERE ack IS NULL OR ack IS FALSE LIMIT 1;
-EXIT WHEN eventId IS NULL;
-EXIT WHEN eventId = 0;
-PERFORM pg_notify(ch, eventId::text);
-i = i + 1;
-END LOOP;
-SELECT 'pg_notify performed ' || i::varchar(255) || ' times.' as msg into return_message;
-RETURN return_message;
+    FOR eventId IN
+    SELECT id FROM notify_events WHERE ack IS NULL OR ack IS FALSE
+    LOOP
+        RETURN NEXT eventId;
+        PERFORM pg_notify(ch, eventId::text);
+    END LOOP;
+    RETURN;
 END;
 $$ LANGUAGE plpgsql;
