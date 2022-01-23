2441849154410e322fa68ecf56b04429fcefa404
Dragusin Mihai <chaotikdarkness@gmail.com>
2022-01-23 23:36:49 +0800

    Listener loop with ack and data scan

--- main.go
+++ main.go
@@ -9,19 +9,66 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
+	"time"
 
-	_ "github.com/lib/pq"
+	"github.com/lib/pq"
 )
 
 var (
-	addr      string
-	dbUser    string
-	dbPass    string
-	dbHost    string
-	dbName    string
-	dbsslMode string
-	DB        *sql.DB
+	addr       string
+	dbUser     string
+	dbPass     string
+	dbHost     string
+	dbName     string
+	dbsslMode  string
+	DB         *sql.DB
+	listener   *pq.Listener
+	eventsFlag string
+	events     []string
 )
+
+func listenNotifyEvents(dsn string) {
+	listener = pq.NewListener(dsn, time.Second*1, time.Second*120, func(event pq.ListenerEventType, err error) {
+		if event == pq.ListenerEventConnected {
+			log.Println("pq listener connected")
+		}
+		if event == pq.ListenerEventConnectionAttemptFailed {
+			log.Println("pq listener connection attempt failed")
+		}
+		if event == pq.ListenerEventDisconnected {
+			log.Println("pq listener disconnected")
+		}
+		if event == pq.ListenerEventReconnected {
+			log.Println("pq listener reconnected")
+		}
+		if err != nil {
+			log.Println(err.Error())
+		}
+	})
+	for k := range events {
+		if err := listener.Listen(events[k]); err != nil {
+			log.Println(err.Error())
+		}
+	}
+	go func() {
+		for {
+			select {
+			case b := <-listener.Notify:
+				log.Printf("channel [%s]: %s", b.Channel, b.Extra)
+				row := DB.QueryRow(fmt.Sprintf("select ack(%s,'%s');", b.Extra, "Go-Consumer-01"))
+				data := new(interface{})
+				if err := row.Scan(&data); err != nil {
+					log.Println("scan: ", err.Error())
+				}
+				// TODO spawn task with deadline set depending on channel
+				log.Println(((*data).(string)))
+			default:
+				time.Sleep(time.Second * 1)
+			}
+		}
+	}()
+}
 
 func connectDB(user, pass, host, database, sslMode string) {
 	var err error
@@ -34,6 +81,7 @@ func connectDB(user, pass, host, database, sslMode string) {
 		log.Println("Failed to establish database connection")
 	}
 	fmt.Println("open postgres connections:", DB.Stats().OpenConnections)
+	listenNotifyEvents(url)
 }
 
 func defineFlags() {
@@ -43,6 +91,7 @@ func defineFlags() {
 	flag.StringVar(&dbHost, "dbhost", "localhost:5432", "database host")
 	flag.StringVar(&dbName, "dbname", "shared_db01", "database name")
 	flag.StringVar(&dbsslMode, "dbsslmode", "require", "database sslMode")
+	flag.StringVar(&eventsFlag, "notify", "basic,fast", "pg_notify event namespace")
 }
 
 func listen(addr string, mux *http.ServeMux) *http.Server {
@@ -73,6 +122,7 @@ func registerHandlers(mux *http.ServeMux, handlers map[string]http.HandlerFunc) 
 func main() {
 	defineFlags()
 	flag.Parse()
+	events = strings.Split(eventsFlag, ",")
 	sigInt := make(chan os.Signal, 1)
 	signal.Notify(sigInt, os.Interrupt)
 	mux := new(http.ServeMux)
@@ -83,4 +133,6 @@ func main() {
 	s := listen(addr, mux)
 	<-sigInt
 	s.Shutdown(context.Background())
+	listener.Close()
+	DB.Close()
 }
--- sql/acknowledge.sql
+++ sql/acknowledge.sql
@@ -1,11 +1,12 @@
-create or replace function ack(eventId integer, consumerId varchar(255)) RETURNS INTEGER AS 
+create or replace function ack(eventId integer, consumerId varchar(255)) RETURNS TEXT AS 
 $func$
 declare row_affected integer;
+declare row_data text;
 BEGIN
 UPDATE notify_events
 SET consumer = consumerId, ack = true 
 WHERE id = eventId AND ack IS NOT TRUE
-RETURNING ID INTO row_affected;
-RETURN row_affected;
+RETURNING ID, DATA INTO row_affected, row_data;
+RETURN row_data;
 END;
 $func$ LANGUAGE plpgsql;
